56371229f7f8df9e66cc8fd8f0c78ceb01e86e83
xiaolong666666 <long.1585097369@qq.com>
2021-04-01 20:26:45 +0800

    发布房源功能完成

--- routes/homestay.js
+++ routes/homestay.js
@@ -11,30 +11,14 @@ const cacheFolder = 'public/images/'
 
 /* GET homestay listing. */
 router.get('/', function (req, res, next) {
-    // const { headers: { host }, body: { phone, password, gender, role } } = req
-    // const avatar = `http://${host}/images/default.jpg`
-    // const user_arr = [role, phone, md5(password), gender, avatar]
     const sqlSelectStr = `SELECT * from homestay`
-    // const sqlInsertStr = `INSERT INTO user(user_role, user_phone, user_password, user_gender, user_avatar) VALUES (?, ?, ?, ?, ?)`
     conn.query(sqlSelectStr, (error, results, fields) => {
         if (error) {
             res.json({ code: 0, message: '很遗憾，获取房屋失败！' })
         } else {
-            res.json({ code: 200, homestay: results })            
-            // if (!isEmpty(results)) {
-            //     res.json({ code: 304, message: '此用户名已存在！' })
-            // } else {
-            //     conn.query(sqlInsertStr, user_arr, (error, results, fields) => {
-            //         if (error) {
-            //             res.json({ code: 0, message: '很遗憾，注册失败！' });
-            //         } else {
-            //             res.json({ code: 200, message: '恭喜你，注册成功！' });
-            //         }
-            //     })
-            // }
+            res.json({ code: 200, homestay: results })
         }
     })
 })
-
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ const cacheFolder = 'public/images/'
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+})
 
 // 用户注册
 router.post('/sign_up', (req, res) => {
@@ -42,15 +42,14 @@ router.post('/sign_up', (req, res) => {
 
 // 用户登录
 router.post('/sign_in', (req, res) => {
-  console.log('req.body', req.body)
   const { body: { phone, password } } = req
   const sqlSelectStr = `SELECT * from user WHERE user_phone = '${phone}' AND user_password = '${md5(password)}'`
   conn.query(sqlSelectStr, (error, results, fields) => {
     if (error) {
       res.json({ code: 0, message: '账号或密码错误！' })
     } else {
-      req.body.password = md5(password)
-      const token = jwt.sign(req.body, token_key, { expiresIn: '2h' })
+      const ssh = { user_id: results[0]["user_id"] }
+      const token = jwt.sign(ssh, token_key, { expiresIn: '2h' })
       if (!isEmpty(results)) {
         res.json({ code: 200, message: '登录成功！', token })
       } else {
@@ -62,7 +61,6 @@ router.post('/sign_in', (req, res) => {
 
 // 用户验证
 router.post('/sign_check', (req, res) => {
-  console.log('req.headers', req.headers)
   const { headers: { authorization } } = req
   jwt.verify(authorization, token_key, (error, decoded) => {
     if (error) {
@@ -72,8 +70,8 @@ router.post('/sign_check', (req, res) => {
         default: res.json({ code: 0, message: 'token无效' })
       }
     } else {
-      const { phone, password } = decoded
-      const sqlSelectStr = `SELECT * from user WHERE user_phone = '${phone}' AND user_password = '${password}'`
+      const { user_id } = decoded
+      const sqlSelectStr = `SELECT * from user WHERE user_id = '${user_id}'`
       conn.query(sqlSelectStr, (error, results, fields) => {
         if (error) {
           res.json({ code: 0, message: '登录状态错误！' })
@@ -121,8 +119,8 @@ router.post('/avatar', function (req, res) {
           default: res.json({ code: 0, message: 'token无效' })
         }
       } else {
-        const { phone } = decoded
-        const avatarName = `/${phone}${Date.now()}.${extName}`
+        const { user_id } = decoded
+        const avatarName = `/${user_id}${Date.now()}.${extName}`
         const newPath = form.uploadDir + avatarName;
         const path = newPath.replace('public', '')
         if (isEmpty(flag)) {
@@ -166,30 +164,115 @@ router.post('/modify_personal_information', (req, res) => {
 
 // 查询房屋
 router.post('/homestay', function (req, res) {
-  const { headers } = req
-  console.log('headers', headers)
-  // const avatar = `http://${host}/images/default.jpg`
-  // const user_arr = [role, phone, md5(password), gender, avatar]
-  const sqlSelectStr = `SELECT * from homestay`
-  // const sqlInsertStr = `INSERT INTO user(user_role, user_phone, user_password, user_gender, user_avatar) VALUES (?, ?, ?, ?, ?)`
-  conn.query(sqlSelectStr, (error, results, fields) => {
+  const { headers: { authorization } } = req
+  jwt.verify(authorization, token_key, (error, decoded) => {
     if (error) {
-      res.json({ code: 0, homestay: [] })
+      console.log('homestayerror', error)
     } else {
-      res.send({ code: 200, homestay: results })
-      // if (!isEmpty(results)) {
-      //     res.json({ code: 304, message: '此用户名已存在！' })
-      // } else {
-      //     conn.query(sqlInsertStr, user_arr, (error, results, fields) => {
-      //         if (error) {
-      //             res.json({ code: 0, message: '很遗憾，注册失败！' });
-      //         } else {
-      //             res.json({ code: 200, message: '恭喜你，注册成功！' });
-      //         }
-      //     })
-      // }
+      const { user_id } = decoded
+      const sqlSelectStr = `SELECT * from homestay WHERE landlord_id = ${user_id}`
+      conn.query(sqlSelectStr, (error, results, fields) => {
+        if (error) {
+          res.json({ code: 0, homestay: [] })
+        } else {
+          results = results.map(item => ({
+            ...item,
+            homestay_facility: JSON.parse(item.homestay_facility),
+            homestay_picture: JSON.parse(item.homestay_picture),
+          }))
+          res.send({ code: 200, homestay: results })
+        }
+      })
     }
   })
+})
+
+// 添加房源图片
+router.post('/homestay/picture', function (req, res) {
+  const userDirPath = cacheFolder + "homestay_picture";
+  if (!fs.existsSync(userDirPath)) {
+    fs.mkdirSync(userDirPath);
+  }
+  const form = new formidable.IncomingForm(); //创建上传表单
+  form.encoding = 'utf-8'; //设置编辑
+  form.uploadDir = userDirPath; //设置上传目录
+  form.keepExtensions = true; //保留后缀
+  form.maxFieldsSize = 2 * 1024 * 1024; //文件大小
+  form.type = true;
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      return res.json({ code: 0, error: err });
+    }
+    let extName = ''; //后缀名
+    switch (files.picture.type) {
+      case 'image/pjpeg': extName = 'jpg'; break;
+      case 'image/jpeg': extName = 'jpg'; break;
+      case 'image/png': extName = 'png'; break;
+      case 'image/x-png': extName = 'png'; break;
+      case 'image/gif': extName = 'gif'; break;
+    }
+    const { headers: { host, authorization } } = req
+    jwt.verify(authorization, token_key, (error, decoded) => {
+      if (error) {
+        switch (error.name) {
+          case 'JsonWebTokenError': res.json({ code: 0, message: 'token无效' }); break;
+          case 'TokenExpireError': res.json({ code: 0, message: 'token过期' }); break;
+          default: res.json({ code: 0, message: 'token无效' })
+        }
+      } else {
+        const { user_id } = decoded
+        const pictureName = `/${user_id}${Date.now()}.${extName}`
+        const newPath = form.uploadDir + pictureName;
+        const path = newPath.replace('public', '')
+        fs.renameSync(files.picture.path, newPath) // 新建图片
+        res.json({ code: 200, pictureUrl: `http://${host}${path}` });
+      }
+    })
+  });
+})
+
+// 添加房源信息
+router.post('/homestay/issue', function (req, res) {
+  const {
+    headers: { authorization },
+    body: { homestay_name, homestay_pirce, homestay_address, homestay_facility, homestay_recommend, homestay_picture }
+  } = req
+  const facilityToJSON = JSON.stringify(homestay_facility)
+  const pictureToJSON = JSON.stringify(homestay_picture)
+  const homestay_arr = [homestay_name, Number(homestay_pirce), homestay_address, facilityToJSON, homestay_recommend, pictureToJSON ]
+  jwt.verify(authorization, token_key, (error, decoded) => {
+    if (error) {
+      switch (error.name) {
+        case 'JsonWebTokenError': res.json({ code: 0, message: 'token无效' }); break;
+        case 'TokenExpireError': res.json({ code: 0, message: 'token过期' }); break;
+        default: res.json({ code: 0, message: 'token无效' })
+      }
+    } else {
+      const { user_id } = decoded
+      const sqlInsertStr = `INSERT INTO homestay(landlord_id, homestay_name, homestay_pirce, homestay_address, homestay_facility, homestay_recommend, homestay_picture) VALUES(?, ?, ?, ?, ?, ?, ?)`
+      homestay_arr.unshift(user_id)
+      conn.query(sqlInsertStr, homestay_arr, (error, results, fields) => {
+        if (error) {
+          console.log('error', error.code)
+          res.json({ code: 0, message: error.code })
+        } else {
+          if (!isEmpty(results)) {
+            console.log('results', results)
+            res.json({ code: 200, message: '发布房源成功！' })
+          } else {
+            res.json({ code: 0, message: '发布房源失败！' })
+          }
+        }
+      })
+    }
+  })
+  
+})
+
+// 修改房源信息
+router.post('/homestay/modify', function (req, res) {
+  const { headers: { authorization }, body } = req
+  console.log('body', body)
 })
 
 module.exports = router
